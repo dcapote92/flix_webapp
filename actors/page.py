4a17866bcc9b2f4fd9bd8910eb9b4bf5c6298948
dcapote92 <danicapote92@gmail.com>
2025-12-10 13:47:01 -0300

    actors integrated with the API

--- actors/page.py
+++ actors/page.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from datetime import datetime
 from st_aggrid import AgGrid
 from actors.service import ActorService
 
@@ -22,7 +23,18 @@ def show_actors():
 
     st.title('Cadastrar novo Ator/Atriz')
     name = st.text_input('Nome do Ator/Atriz')
+    birthday = st.date_input(
+        label='Data de nascimento',
+        value=datetime.today(),
+        min_value=datetime(1900, 1, 1).date(),
+        max_value=datetime.today(),
+        format='DD/MM/YYYY'
+    )
+    nationality = st.text_input('Nacionalidade')
 
     if st.button('Cadastrar'):
-        actors.append
-        st.success(f'Ator/Atriz "{name}" cadastrado com sucesso!')
+        new_actor = actors_service.create_actor(name, birthday, nationality)
+        if new_actor:
+            st.rerun()
+        else:
+            st.error('Erro ao cadastrar Ator/Atriz')
---
+++ actors/service.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from actors.repository import ActorRepository
+
+
+class ActorService:
+
+    def __init__(self):
+        self.actor_repository = ActorRepository()
+
+    def get_actors(self):
+        return self.actor_repository.get_actors()
+
+    def get_actors_name(self):
+        actors = self.get_actors()
+        actors_name = [actors.get('name') for actor in actors]
+        return actors_name
+
+    def create_actor(self, name: str, birthday: datetime, nationality: str):
+        actor = dict(
+            name=name,
+            birthday=birthday,
+            nationality=nationality
+        )
+        return self.actor_repository.create_actor(actor)
