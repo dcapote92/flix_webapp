9867dafbdc9a3cb9226921e94f026720d9c1cb2a
dcapote92 <danicapote92@gmail.com>
2025-12-10 09:52:19 -0300

    connecting frontend with api, api module created

--- api/service.py
+++ api/service.py
@@ -5,12 +5,12 @@ class Auth:
 
     def __init__(self):
         self.__base_url = 'https://dcapote92.pythonanywhere.com/api/v1/'
-        self.__auth_url = f'{self.base_url}authentication/token/'
+        self.__auth_url = f'{self.__base_url}authentication/token/'
 
     def get_token(self, user: str, password: str) -> str:
 
         auth_payload = {
-            'user': user,
+            'username': user,
             'password': password
         }
 
@@ -19,7 +19,7 @@ class Auth:
             data=auth_payload
         )
 
-        if auth_payload.status_code == 200:
+        if auth_response.status_code == 200:
             return auth_response.json()
 
         return {'error': f'Erro ao autenticar. Status code: {auth_response.status_code}'}
